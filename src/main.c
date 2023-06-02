7b48f9b4d5a98e05482eaae7bd5e96ef6dfc2b7e
Jacob Hall <jachallbound@gmail.com>
2023-06-02 19:29:17 -0400

    using new functions

--- src/main.c
+++ src/main.c
@@ -1,11 +1,23 @@
 #include <curses.h>
 #include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "data_structures.h"
-#include "movement.h"
+#include "data_input.h"
+#include "keyboard_input.h"
+#include "screen.h"
 
 /* Global */
 state game_state = RUNNING;
 int X = 0, Y = 0;
+
+/* Define possible map names */
+char* MAP00 = "sr/data/map00.dat";
+char* MAP01 = "src/data/map01.dat";
+char* MAP02 = "src/data/map02.dat";
+char* MAP03 = "src/data/map03.dat";
+char* MAP04 = "src/data/map04.dat";
 
 /* Main */
 int main(int argc, char** argv) {
@@ -26,8 +38,27 @@ int main(int argc, char** argv) {
   X = (term_X < X ? term_X : X); /* Reduce X,Y to terminal size if too large */
   Y = (term_Y < Y ? term_Y : Y);
 
-  /* Draw play area and set cursor to 1,1 */
+  /* Load map from file and set state */
+  char map_characters[MAX_MAP_SIZE];
+  cell map_cells[MAX_X][MAX_Y];
+  size_t map_file_length = load_map_characters(MAP00, map_characters);
+  if (map_file_length == 0) {
+    perror("Map was not loaded from file");
+    endwin();
+    return -1;
+  }
+  int map_conversion_status = convert_map_to_cells(map_characters, map_cells, map_file_length);
+  if (map_conversion_status < 0) { 
+    perror("Failed to convert map characters to cells");
+    endwind();
+    return -1;
+  }
+
+
+  /* Old, Draw play area and set cursor to 1,1 */
   draw_play_area(X, Y);
+
+  /* Get starting cursor position */
   int x = getcurx(wnd);
   int y = getcury(wnd);
   position xy;
@@ -35,11 +66,24 @@ int main(int argc, char** argv) {
   xy.y = y;
 
   /* Game loop */
-  while(game_state) {
-    /* Get input */
-    c = getch();
-    handle_input(wnd, &xy, &c);
-  }
+  do {
+    switch (game_state) {
+      case ENDING:
+        /* Game will end */
+        break;
+      case RUNNING:
+      case PAUSED:
+        /* Get input */
+        c = getch();
+        handle_input(wnd, &xy, &c);
+        break;
+      case CHANGING_MAP:
+
+        game_state = RUNNING;
+        break;
+    }
+    
+  } while(game_state);
 
   /* Close curses */
   endwin();
