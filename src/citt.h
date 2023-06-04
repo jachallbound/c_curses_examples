8eb1e1abd48e504d9b92b503bb674cbc1c19c7d7
Jacob Hall <jachallbound@gmail.com>
2023-06-04 14:48:40 -0400

    encompassing header

---
+++ src/citt.h
@@ -0,0 +1,11 @@
+#ifndef CITT_H
+#define CITT_H
+
+/* All CitT Libraries */
+#include "data_structures.h"
+#include "data_input.h"
+#include "keyboard_input.h"
+#include "screen.h"
+#include "entity_handling.h"
+
+#endif /* CITT_H */
